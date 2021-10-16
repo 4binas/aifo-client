6bb634034e0367452fc87a4f7b15e23fe926d116
Abinas Kuganathan <abinas.kuganathan@ost.ch>
2021-10-16 14:07:23 +0200

    init

---
+++ app.py
@@ -0,0 +1,38 @@
+import os
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "E:/User/Documents/GitHub/AIFo/aifo-client/key.json"
+
+
+def detect_intent_texts(project_id, session_id, texts, language_code):
+    """Returns the result of detect intent with texts as inputs.
+
+    Using the same `session_id` between requests allows continuation
+    of the conversation."""
+    from google.cloud import dialogflow
+
+    session_client = dialogflow.SessionsClient()
+
+    session = session_client.session_path(project_id, session_id)
+    print("Session path: {}\n".format(session))
+
+    for text in texts:
+        text_input = dialogflow.TextInput(text=text, language_code=language_code)
+
+        query_input = dialogflow.QueryInput(text=text_input)
+
+        response = session_client.detect_intent(
+            request={"session": session, "query_input": query_input}
+        )
+
+        print("=" * 20)
+        print("Query text: {}".format(response.query_result.query_text))
+        print(
+            "Detected intent: {} (confidence: {})\n".format(
+                response.query_result.intent.display_name,
+                response.query_result.intent_detection_confidence,
+            )
+        )
+        print(response)
+        print("Fulfillment text: {}\n".format(response.query_result.fulfillment_text))
+
+
+detect_intent_texts("aifo-project-jpdg", "abcde", ["Tell me a joke"], "en-US")
